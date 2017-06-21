ffaaf6329f8f72cd3f3b5c810f38780c3c28bd61
Heri Agape <agape@live.fr>
2017-06-21 11:04:53 +0300

    list view showing

--- app/src/main/java/co/heri/dev/opennote/MainActivity.java
+++ app/src/main/java/co/heri/dev/opennote/MainActivity.java
@@ -1,20 +1,22 @@
 package co.heri.dev.opennote;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import co.heri.dev.opennote.Model.Task;
 
 public class MainActivity extends AppCompatActivity {
     private List<Task> taskList = new ArrayList<>();
-    private RecyclerView task_recycler;
     private ListView task_ListView;
 
     @Override
@@ -23,17 +25,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         task_ListView = (ListView) findViewById(R.id.task_list_view);
+
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        /*final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);*/
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.task, R.id.firstLine, values);
+
+        task_ListView.setAdapter(adapter);
         
 
 
 
         // TODO: Read shared toekn and email from shared perefence.
-        Intent intent = getIntent();
+        /*Intent intent = getIntent();
         String ueamil = intent.getStringExtra(LoginActivity.USER_TOKEN);
 
         TextView textView = (TextView) findViewById(R.id.hello_message);
 
-        textView.setText("Token Details: " + ueamil);
+        textView.setText("Token Details: " + ueamil);*/
+
+
+        task_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 
 
@@ -41,3 +73,29 @@ public class MainActivity extends AppCompatActivity {
         // load the tasks
     }
 }
+
+class StableArrayAdapter extends ArrayAdapter<String> {
+
+    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+    public StableArrayAdapter(Context context, int textViewResourceId,
+                              List<String> objects) {
+        super(context, textViewResourceId, objects);
+        for (int i = 0; i < objects.size(); ++i) {
+            mIdMap.put(objects.get(i), i);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        String item = getItem(position);
+        return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+}
+
